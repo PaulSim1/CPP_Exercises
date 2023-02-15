27aaf11d86f0524a7a725d191c94c0bf73bfdd02
Varchat <paulsim777@gmail.com>
2023-02-15 08:36:48 +0100

    tp4

--- src/Pokeball.h
+++ src/Pokeball.h
@@ -1,5 +1,33 @@
+
 #pragma once
 
+#include "Pokemon.h"
+
+#include <memory>
+
 // A ball where a Pokemon sleeps.
-class Pokeball
-{};
+class Pokeball {
+
+public:
+
+    bool empty() const{
+        return _pokemon == nullptr;
+    }
+
+    void store (PokemonPtr pokemon) {
+        // move permet d'affecter un pointeur à un autre, on perd dans un pour mettre dans l'autre
+        _pokemon = std::move(pokemon);
+    }
+
+    // accesseur, depuis le pointeur on peut chopper le pokémon
+    const Pokemon& pokemon() const {
+        return *_pokemon;
+    }
+
+
+private:
+    
+    // pointeur pour les pokémons, défini dans Pokémon.h
+    PokemonPtr _pokemon;
+
+};
--- src/Pokemon.h
+++ src/Pokemon.h
@@ -1,7 +1,50 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 // A creature that is cute and can fight other ones.
-class Pokemon
-{};
+class Pokemon {
+
+public:
+
+    Pokemon(const std::string& name)
+        : _name {name}
+        , _id {next_id++}
+    {}
+
+    // pour que les copies permettent d'incrémenter les id
+    Pokemon(const Pokemon& other)
+        : _name { other._name }
+        , _id { next_id++ }
+    {}
+    Pokemon& operator=(const Pokemon& other){
+        if (this == &other)
+            _name = other._name;
+        
+        return *this;
+    } 
+
+    // premier const et & pour avoir une référence, permet de renvoyer un pointeur, moind lourd que toute la chaine
+    // deuxieme const pour créer un pokémon sous forme de constante
+    const std::string& name() const {
+        return _name;
+    }
+
+    // const pour créer un pokémon sous forme de constante
+    int id() const {
+        return _id;
+    }
+
+
+private:
+    
+    static inline int next_id = 0;
+
+    std::string _name;
+    int _id = 0;
+
+};
+
+// pointeur pour les pokémons
+using PokemonPtr = std::unique_ptr<Pokemon>;
